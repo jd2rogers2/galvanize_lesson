df23f5cdd8d6cff20d328475748afefaa5302652
James Rogers <jd2rogers2@gmail.com>
2022-07-12 16:18:52 -0700

    what's wrong with my gitignore? function stubs and test env running

---
+++ index.js
@@ -0,0 +1,9 @@
+const transposer = function* (twoDArr) {};
+const pairwiser = function* (arr) {};
+const findPair = function* (arr, sum) {};
+
+module.exports = {
+  transposer,
+  pairwiser,
+  findPair
+}
---
+++ index.test.js
@@ -0,0 +1,37 @@
+const {
+  transposer,
+  pairwiser,
+  findPair
+} = require('./index');
+
+/**
+The first learning objective would create a generator function that accomplishes the following:
+
+Input: A 2-d rectangular list. An example is [[1, 2, 3], [6, 5, 4]].
+Output: The sequence of the transpose. For the given example, the sequence is 1, 6, 2, 5, 3, 4.
+
+The second learning objective would transform a list into pairwise sums of contiguous entries:
+
+Input: A list of values. An example is [1, 9, 2, 4, 1, 4].
+Output: The pairwise sums of the elements. For the given example, the return value is a list containing [10, 11, 6, 5, 5].
+
+The third learning objective would find two consecutive entries in a list (it doesn’t have to be unique) that sum to a specified value.
+
+Input: A list of values and a target sum. An example is a list [1, 9, 2, 4, 7, 4] and a specified target sum of 11.
+Output: The procedure will identify any of these pairs: (9, 2), (2, 9), (4, 7), or (7, 4)
+ */
+
+describe('let\'s learn generator functions* !!', () => {
+  describe('transposer', () => {
+    it('does a thing', () => {});
+  });
+
+  describe('pairwiser', () => {
+    it('does a thing', () => {});
+  });
+
+  describe('findPair', () => {
+    it('does a thing', () => {});
+  });
+
+});
