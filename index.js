ddc47e6a32df3a427fbfb2ea1a086388e9a51307
James Rogers <jd2rogers2@gmail.com>
2022-07-12 18:08:37 -0700

    get transposer working

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-const transposer = function* (twoDArr) {};
-const pairwiser = function* (arr) {};
-const findPair = function* (arr, sum) {};
+function* transposer(twoDArr) {
+  let outerInd = -1;
+  let innerInd = -1;
+
+  while (true) {
+    outerInd += 1;
+    if (outerInd > twoDArr.length - 1) {
+      outerInd = 0;
+    }
+    if (outerInd === 0) {
+      innerInd += 1;
+    }
+
+    yield twoDArr[outerInd][innerInd];
+  }
+};
+function* pairwiser(arr) {};
+function* findPair(arr, sum) {};
 
 module.exports = {
   transposer,
